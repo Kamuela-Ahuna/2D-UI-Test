4d470570aaf8b739fc1101f45ab3e513f6189f52
Kamuela Ahuna <ahunak@student.swosu.edu>
2020-01-23 09:43:56 -0600

    Moved Object
    Just a test

---
+++ Assets/Scripts/UITesting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UITesting : MonoBehaviour
+{
+    public TextMeshProUGUI textBox;
+    public string txtString;
+    public int buildIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void ChangeText()
+    {
+        textBox.text = txtString;
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
